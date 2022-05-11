67196e132ab5e019f0b2bfe25764350494644cf6
Jasurbek <jasurisoqov685@gmail.com>
2022-05-11 10:21:16 +0500

    fixed issues 1 and sucses

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 "use script";
 
-let nums = [1, 9, 3, 4];
-let target = 5;
+let nums = [3, 2, 4];
+// 9
+// [3,2,4]
+// 6
+// [3,3]
+// 6];
+let target = 6;
 
 var twoSum = function (nums, target) {
   for (let i = 0; i < nums.length; i++) {
-    for (let j = i; j < nums.length - i; j++) {
-      if (nums[i] + nums[j] === target) {
+    for (let j = i; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target && i !== j) {
         return [i, j];
       }
     }
