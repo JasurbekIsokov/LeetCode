d54b28268772c813a5883b92123a6c42ccdaa2e8
Jasurbek <jasurisoqov685@gmail.com>
2022-06-06 16:01:43 +0500

    issue solution

--- script.js
+++ script.js
@@ -423,4 +423,22 @@
 
 // ////////////////////////////////////////////////////////////////////////
 
-var climbStairs = function (n) {};
+n = 4;
+
+var climbStairs = function (n) {
+  if (n <= 1) {
+    return 1;
+  }
+
+  let ways = [];
+  ways[0] = 1;
+  ways[1] = 1;
+
+  for (let i = 2; i <= n; i++) {
+    ways[i] = ways[i - 2] + ways[i - 1];
+  }
+
+  return ways[n];
+};
+
+console.log(climbStairs(n));
