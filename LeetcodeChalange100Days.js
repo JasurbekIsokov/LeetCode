b45ad885825f1d855d245cc3e8a8b4c844d5a861
Jasurbek <jasurisoqov685@gmail.com>
2024-05-21 20:24:16 +0300

    Leetcode Chalange 100 days - first day 2 issues solced

---
+++ LeetcodeChalange100Days.js
@@ -0,0 +1,25 @@
+"use strict";
+
+// 2635. Apply Transform Over Each Element in Array
+
+// var map = function (arr, fn) {
+//   for (let i = 0; i < arr.length; i++) {
+//     arr[i] = fn(arr[i], i);
+//   }
+
+//   return arr;
+// };
+
+// 2634. Filter Elements from Array
+
+// var filter = function (arr, fn) {
+//   let filteredArr = [];
+
+//   for (let i = 0; i < arr.length; i++) {
+//     if (fn(arr[i], i)) {
+//       filteredArr.push(arr[i]);
+//     }
+//   }
+
+//   return filteredArr;
+// };
