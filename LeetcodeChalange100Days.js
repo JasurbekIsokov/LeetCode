076f9ddebf730163d0b34721c86256ea3430e597
Jasurbek <jasurisoqov685@gmail.com>
2024-05-26 09:03:11 +0300

    Remove Element

--- LeetcodeChalange100Days.js
+++ LeetcodeChalange100Days.js
@@ -23,3 +23,19 @@
 
 //   return filteredArr;
 // };
+
+// 27. Remove Element
+
+function removeElement(nums, val) {
+  let j = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== val) {
+      nums[j] = nums[i];
+      j++;
+    }
+  }
+
+  return j;
+}
+
+console.log(removeElement([3, 2, 2, 3], 3));
